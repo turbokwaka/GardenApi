b8430f9480e7279daab887beff43117dd96555e0
artem <qvikhoney@gmail.com>
2024-05-08 13:22:15 +0300

    init with one get request

---
+++ GardenApi/Controllers/PlantsControllers.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GardenApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlantsController : ControllerBase
+    {
+        private readonly string _key = "sk-IezH6630ee7f8f86e5294";
+        
+        [HttpGet("plant/search")]
+        public async Task<IActionResult> GetPlantability([FromQuery] string keyword)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = $"https://perenual.com/api/species-list?key={_key}&q={keyword}";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                
+                string json = await response.Content.ReadAsStringAsync();
+                Root plantData = JsonConvert.DeserializeObject<Root>(json);
+                
+                string jsonResponse = JsonConvert.SerializeObject(plantData);
+
+                // Create an HTTP content with JSON data
+                var content = new ContentResult
+                {
+                    Content = jsonResponse,
+                    ContentType = "application/json",
+                    StatusCode = 200 // OK status code
+                };
+
+                return content;
+            }
+        }
+    }
+}
---
+++ GardenApi/Plant.cs
@@ -0,0 +1,22 @@
+﻿namespace GardenApi;
+
+public class Datum
+{
+    public string common_name;
+    public List<string> scientific_name;
+    public List<string?> other_name;
+    public string cycle;
+    public string watering;
+    public object sunlight;
+    public DefaultImage default_image;
+}
+
+public class DefaultImage
+{
+    public string original_url;
+}
+
+public class Root
+{
+    public List<Datum> data;
+}
